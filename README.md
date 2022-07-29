12dae22401d6751bf1de92eb74258ba7cb2e17c6
dengjinlong <dengjinlong@jtexpress.com>
2022-07-29 17:34:53 +0800

    merge desc to schema

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # APIModel
 Generate api model from RAP2操作平台
+
+$ npm install -g APIModel
+$ APIModel COMMAND
+running command...
+$ APIModel (--version)
+APIModel/0.0.0 darwin-x64 node-v18.4.0
+$ APIModel --help [COMMAND]
+USAGE
+  $ APIModel COMMAND
+```
---
+++ src/commands/schema/quicktype.ts
@@ -0,0 +1,76 @@
+const {
+  quicktype,
+  InputData,
+  jsonInputForTargetLanguage,
+  JSONSchemaInput,
+  FetchingJSONSchemaStore,
+} = require('quicktype-core');
+
+// json to code
+export async function quicktypeJSON(targetLanguage: string, typeName: string, jsonString: string) {
+  const jsonInput = jsonInputForTargetLanguage(targetLanguage);
+
+  // We could add multiple samples for the same desired
+  // type, or many sources for other types. Here we're
+  // just making one type from one piece of sample JSON.
+  await jsonInput.addSource({
+    name: typeName,
+    samples: [jsonString],
+  });
+
+  const inputData = new InputData();
+  inputData.addInput(jsonInput);
+
+  return await quicktype({
+    inputData,
+    lang: targetLanguage,
+  });
+}
+
+// schema to code
+export async function quicktypeJSONSchema(targetLanguage: string, typeName: string, jsonSchemaString: string) {
+  const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
+
+  // We could add multiple schemas for multiple types,
+  // but here we're just making one type from JSON schema.
+  await schemaInput.addSource({ name: typeName, schema: jsonSchemaString });
+
+  const inputData = new InputData();
+  inputData.addInput(schemaInput);
+
+  var rendererOptions = {}
+  if (targetLanguage == "swift") {
+    rendererOptions = {
+      "just-types":"true",
+      "density": "normal",
+      "initializers": "false",
+      "coding-keys": "false",
+      "protocol": "jtcustom",
+      "struct-or-class": "class",
+      "mutable-properties": "true",
+      "use-default-value": "true",
+    }
+  }
+
+  return await quicktype({
+    inputData,
+    lang: targetLanguage,
+    rendererOptions
+  });
+}
+
+async function main() {
+  const { lines: swiftPerson } = await quicktypeJSON(
+    "swift",
+    "Person",
+    "jsonString"
+  );
+  console.log(swiftPerson.join("\n"));
+
+  const { lines: pythonPerson } = await quicktypeJSONSchema(
+    "python",
+    "Person",
+    "jsonSchemaString"
+  );
+  console.log(pythonPerson.join("\n"));
+}
